fe377fda51733491817ba7be2118eb053caccb09
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-04-26 11:47:12 -0300

    import header on _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/global.scss';
 
+import { Header } from '../components/Header';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
