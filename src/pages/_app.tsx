e45d8966bc18697704f432e46947ea44ee48f516
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-04-26 11:50:24 -0300

    import app atyles on _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
-import '../styles/global.scss';
-
 import { Header } from '../components/Header';
+
+import '../styles/global.scss';
+import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <Header />
-      <Component {...pageProps} />
+    <div className={styles.appWrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
     </div>
   );
 }
