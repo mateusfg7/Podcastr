c0309c7cf4bca6d1012c92ebeb224fccdd9e22ea
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-04-29 11:31:09 -0300

    create isPlaying state on _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,14 +11,18 @@ import styles from '../styles/app.module.scss';
 function MyApp({ Component, pageProps }) {
   const [episodeList, setEpisodeList] = useState([]);
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   function play(episode) {
     setEpisodeList([episode]);
     setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
   }
 
   return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+    <PlayerContext.Provider
+      value={{ episodeList, currentEpisodeIndex, play, isPlaying }}
+    >
       <div className={styles.wrapper}>
         <main>
           <Header />
