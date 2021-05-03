405bff63a164d6322e7a520014ee5d85a2e9ae70
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-05-03 11:32:13 -0300

    use dynamic audio time on player component

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 import { usePlayer } from '../../context/PlayerContext';
 
 import styles from './styles.module.scss';
@@ -79,7 +80,7 @@ export function Player() {
               <div className={styles.emptySlider} />
             </div>
           )}
-          <span>00:00</span>
+          <span>{convertDurationToTimeString(episode?.duration ?? 0)}</span>
         </div>
 
         {episode && (
